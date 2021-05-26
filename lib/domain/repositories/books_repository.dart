3aa35b0d98f56b4b321a0d08dcf6e3d8d08d64b9
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-25 22:35:13 -0600

    Agregando el metodo al repositorio

--- lib/domain/repositories/books_repository.dart
+++ lib/domain/repositories/books_repository.dart
@@ -1,1 +1,7 @@
-abstract class BooksRepository {}
+import 'package:clean_books/core/errors/failure.dart';
+import 'package:clean_books/domain/entities/book.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class BooksRepository {
+  Future<Either<Failure, Book>> getRandomBook();
+}
