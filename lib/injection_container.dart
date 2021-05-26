a63f96bc70a2e097dd761c5ee6da79873256e109
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-26 08:34:59 -0600

    Haciendo mas clean la parte de UI

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,7 @@
-import 'package:clean_books/data/datasources/books_remote_datasource.dart';
+import 'package:clean_books/data/datasources/book_detail/books_remote_datasource.dart';
 import 'package:clean_books/data/repository/books_repository_impl.dart';
 import 'package:clean_books/domain/repositories/books_repository.dart';
-import 'package:clean_books/domain/usecases/get_random_book.dart';
+import 'package:clean_books/domain/usecases/book_detail/get_random_book.dart';
 import 'package:clean_books/presentation/cubit/book_detail/book_detail_cubit.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
@@ -15,7 +15,7 @@ Future<void> init() async {
 Future<void> initBookDetail() async {
   //! Features - BookDetail
   // Cubit
-  sl.registerFactory(() => BookdetailCubit(
+  sl.registerFactory(() => BookDetailCubit(
         sl(),
       ));
 
---
+++ lib/presentation/views/book_detail/book_detail_view.dart
@@ -0,0 +1,113 @@
+import 'package:clean_books/domain/entities/book.dart';
+import 'package:clean_books/presentation/cubit/book_detail/book_detail_cubit.dart';
+import 'package:clean_books/presentation/widgets/action_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../injection_container.dart';
+
+class BookDetailView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return BlocProvider(
+      create: (_) => sl<BookDetailCubit>(),
+      child: BlocBuilder<BookDetailCubit, BookDetailState>(
+        builder: (context, state) {
+          if (state is BookdetailInitial) {
+            _getRandomBook(context);
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (state is BookDetailLoading) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (state is BookDetaiLoaded) {
+            final book = state.book;
+            return _buildBookDetail(size, book, context);
+          } else if (state is BookDetailError) {
+            return Container(
+              child: Center(
+                child: Text(state.message),
+              ),
+            );
+          } else {
+            return Container(
+              child: Center(
+                child: Text('Error'),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  void _getRandomBook(BuildContext context) {
+    final bookDetailCubit = context.read<BookDetailCubit>();
+    bookDetailCubit.getNewRandomBook();
+  }
+
+  Widget _buildBookDetail(Size size, Book book, BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image(
+            image: NetworkImage(book.data.first.image),
+            height: size.height * 0.33,
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          book.data.first.title,
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          book.data.first.author,
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ActionButton(icon: Icons.star_border_rounded),
+            ActionButton(icon: Icons.mode_comment_outlined),
+            ActionButton(icon: Icons.ios_share),
+          ],
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: Card(
+            margin: EdgeInsets.zero,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Description'),
+                    ElevatedButton(
+                        onPressed: () => _getRandomBook(context),
+                        child: Text('Next')),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(book.data.first.description),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
