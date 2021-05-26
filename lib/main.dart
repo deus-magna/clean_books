488929d1a00a9823fbb8e16190f3caa2d50cf7b9
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-25 23:18:02 -0600

    Se agrega el UI para ver los libros

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clean_books/presentation/views/book_detail/book_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,16 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: BookDetailScreen(),
     );
   }
 }
---
+++ lib/presentation/views/book_detail/book_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BookDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Hola mundo jeje'),
+        ),
+      ),
+    );
+  }
+}
